edd9702cc29ad5a10dc9928085dbfa144f9fb64f
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-09 09:27:32 -0500

    more on field and boar

--- app/battleship.js
+++ app/battleship.js
@@ -1,13 +1,35 @@
 class Boat{
     locations;
-    hitLocations = Array();
+    hitLocations = [];
     isSunk = false;
+    hp;
 
-    constructor(locations) {
+    constructor(locations, hp) {
         this.locations = locations;
+        this.hp = hp;
     }
 
     registerHit(pointX, pointY) {
         this.hitLocations.push((pointX, pointY));
+        this.hp -= 1;
+        if (hp == 0) {
+            this.isSunk = true;
+        }
+    }
+}
+
+class Field{
+    hitLocations = [];
+    field;
+
+    constructor(field) {
+        this.field = field;
+    }
+
+    registerHit(pointX, pointY) {
+        this.hitLocations.push((pointX, pointY));
+        if (field[pointX][pointY]) {
+            field[pointX][pointY].registerHit(pointX, pointY);
+        }
     }
 }
