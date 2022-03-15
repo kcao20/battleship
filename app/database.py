6c7d0e26221ace16f5890bb5d64b34729747e38f
Austin Ngan <angan20@stuy.edu>
2022-03-15 09:24:37 -0400

    added check for duplicate usernames in database

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ DB_FILE = "database.db"
 db = sqlite3.connect(DB_FILE)
 cur = db.cursor()
 
+
 cur.execute("""
 	CREATE TABLE IF NOT EXISTS usersClassic(
 		username TEXT,
@@ -13,14 +14,7 @@ cur.execute("""
 		shotsMissed INTEGER,
 		shotsLanded INTEGER
 		)""")
-	
-cur.execute("""
-	CREATE TABLE IF NOT EXISTS leaderboardClassic(
-		username TEXT,
-		winRatio REAL,
-		hitRatio REAL,
-		gamesPlayed INTEGER,
-		gamesWon INTEGER)""")
+
 
 cur.execute("""
 	CREATE TABLE IF NOT EXISTS usersModern(
@@ -32,15 +26,6 @@ cur.execute("""
 		shotsLanded INTEGER,
 		powersUsed INTEGER
 		)""")
-	
-cur.execute("""
-	CREATE TABLE IF NOT EXISTS leaderboardModern(
-		username TEXT,
-		winRatio REAL,
-		hitRatio REAL,
-		powersUsed INTEGER,
-		gamesPlayed INTEGER,
-		gamesWon INTEGER)""")
 
 db.commit()
 db.close()
@@ -48,8 +33,10 @@ db.close()
 def add_userC(username):
 	db = sqlite3.connect(DB_FILE)
 	c = db.cursor()
-	
-	c.execute("""INSERT INTO usersClassic(username,gamesplayed,gameswon,gameslost,shotsmissed,shotslanded) VALUES(?, ?, ?, ?, ?, ?)""", (username,0,0,0,0,0))
+
+	#need to add check for duplicate names
+
+	c.execute("""INSERT INTO usersClassic(username,gamesPlayed,gamesWon,gamesLost,shotsMissed,shotsLanded) VALUES(?, ?, ?, ?, ?, ?)""", (username,0,0,0,0,0))
 	db.commit()
 	db.close()
 	return True
@@ -63,8 +50,9 @@ def add_userM(username):
 	
 	if check is not None:
 		return False
-		
-	c.execute("""INSERT INTO usersModern(username,gamesplayed,gameswon,gameslost,shotsmissed,shotslanded,powersused) VALUES(?,?,?,?,?,?,?)""", (username,0,0,0,0,0,0))
+	
+	else:
+		c.execute("""INSERT INTO usersModern(username,gamesPlayed,gamesWon,gamesLost,shotsMissed,shotsLanded,powersUsed) VALUES(?,?,?,?,?,?,?)""", (username,0,0,0,0,0,0))
 	db.commit()
 	db.close()
 	return True
@@ -82,11 +70,11 @@ def add_statsC(username,win,shotsHit,shotsMissed):
 					WHERE username = ?""", (username,))
 		c.execute("""
 					UPDATE usersClassic
-						SET gameswon = gameswon + 1
+						SET gamesWon = gamesWon + 1
 					WHERE username = ?""", (username,))
 		c.execute("""
 					UPDATE usersClassic
-						SET shotslanded = shotslanded + ?
+						SET shotsLanded = shotsLanded + ?
 					WHERE username = ?""", (shotsHit,username))
 		c.execute("""
 					UPDATE usersClassic
@@ -103,7 +91,7 @@ def add_statsC(username,win,shotsHit,shotsMissed):
 					WHERE username = ?""", (username,))
 		c.execute("""
 					UPDATE usersClassic
-						SET shotslanded = shotslanded + ?
+						SET shotsLanded = shotsLanded + ?
 					WHERE username = ?""", (shotsHit,username))
 		c.execute("""
 					UPDATE usersClassic
@@ -113,7 +101,7 @@ def add_statsC(username,win,shotsHit,shotsMissed):
 	db.close()
 	return True
 
-def add_statsC(username,win,shotsHit,shotsMissed,powersUsed):
+def add_statsM(username,win,shotsHit,shotsMissed,powersUsed):
 	db = sqlite3.connect(DB_FILE)
 	c = db.cursor()
 	
@@ -139,14 +127,24 @@ def add_statsC(username,win,shotsHit,shotsMissed,powersUsed):
 		c.execute("""
 					UPDATE usersClassic
 						SET gamesPlayed = gamesPlayed + 1
+					WHERE username = ?""", (username,))
+		c.execute("""
+					UPDATE usersClassic
 						SET gamesLost = gamesLost + 1
+					WHERE username = ?""", (username,))
+		c.execute("""
+					UPDATE usersClassic
 						SET shotsLanded = shotsLanded + ?
-						SET shotsMissed = shotsMissed + ? 
-					WHERE username = ?""", (shotsHit, shotsMissed, username,))
+					WHERE username = ?""", (shotsHit,username))
+		c.execute("""
+					UPDATE usersClassic
+						SET shotsMissed = shotsMissed + ?
+					WHERE username = ?""", (shotsMissed,username))
 	db.commit()
 	db.close()
 	return True
 
-#add_userM("test")
-#add_userC("test")
-#add_statsC("test",True,3,7)
+add_userM("test")
+add_userC("test")
+add_statsC("test",True,3,7)
+add_userM("test")
